da6d575f3e312fab4d711a1dc41b5c8292cb76aa
SrjAdhikari <surajadhikari055@gmail.com>
2024-08-03 17:08:13 +0900

    Learning bootstrap along with react

---
+++ 03_Bootstrap/src/App.jsx
@@ -0,0 +1,42 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div>
+			<h1>Learning Bootstrap along with React</h1>
+			<h4>Bootstrap Buttons : </h4>
+			<br />
+
+			<button type="button" class="btn btn-primary">
+				Primary
+			</button>
+			<button type="button" class="btn btn-secondary">
+				Secondary
+			</button>
+			<button type="button" class="btn btn-success">
+				Success
+			</button>
+			<button type="button" class="btn btn-danger">
+				Danger
+			</button>
+			<button type="button" class="btn btn-warning">
+				Warning
+			</button>
+			<button type="button" class="btn btn-info">
+				Info
+			</button>
+			<button type="button" class="btn btn-light">
+				Light
+			</button>
+			<button type="button" class="btn btn-dark">
+				Dark
+			</button>
+
+			<button type="button" class="btn btn-link">
+				Link
+			</button>
+		</div>
+	);
+}
+
+export default App;
