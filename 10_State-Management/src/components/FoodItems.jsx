c98dc2eb0cb1d079871ef9439966e20b031c187e
SrjAdhikari <surajadhikari055@gmail.com>
2024-08-11 13:44:54 +0900

    Add functionality to update state when 'Buy' button is clicked

--- 10_State-Management/src/components/FoodItems.jsx
+++ 10_State-Management/src/components/FoodItems.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import Item from "./Item";
 
 // Define the FoodItems functional component
 const FoodItems = (props) => {
 	// Destructure the items array from props
 	const { items } = props;
+
+	// Initialize state to keep track of active items
+	let [activeItems, setActiveItems] = useState([]);
+
+	// Event handler for the "Buy" button click
+	let onBuyButton = (item, event) => {
+		// Create a new array with the current list of active items and the new item
+		let newItems = [...activeItems, item];
+		// Update the state with the new array of active items
+		setActiveItems(newItems);
+	};
 
 	return (
 		// Unordered list with Bootstrap styling for a list group
@@ -15,11 +27,10 @@ const FoodItems = (props) => {
 					key={item}
 					// Pass the current item as a prop to the Item component
 					foodItem={item}
-					// Pass a function as a prop to handle the "Buy" button click
-					handleBuyButton={() => {
-						// Log a message indicating that the item has been bought
-						console.log(`${item} bought.`);
-					}}
+					// Determine if the current item is active and pass it as a prop
+					isBuyClicked={activeItems.includes(item)}
+					// Pass a function to handle the "Buy" button click, including the current item and event
+					handleBuyButton={(event) => onBuyButton(item, event)}
 				/>
 			))}
 		</ul>
