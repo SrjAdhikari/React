420122a53df04749714a27f449318898457a1a74
SrjAdhikari <surajadhikari055@gmail.com>
2024-08-24 23:05:01 +0900

    Integrate PostListProvider context into the App component and manage selected tab state for conditional rendering

--- Projects/07_Social-Media/src/App.jsx
+++ Projects/07_Social-Media/src/App.jsx
@@ -6,30 +6,33 @@ import CreatePost from "./components/CreatePost";
 import PostList from "./components/PostList";
 import "./App.css";
 import { useState } from "react";
+import PostListProvider from "./store/post-list-store";
 
 function App() {
 	// State to keep track of the currently selected tab (either "Home" or "Create Post")
 	const [selectedTab, setSelectedTab] = useState("Home");
 
 	return (
-		<div className="app-container">
-			<Sidebar
-				// `selectedTab` prop: This is used to determine which tab is currently active in the Sidebar.
-				selectedTab={selectedTab}
-				// `setSelectedTab` prop: This function is used to update the `selectedTab` state in the parent component (App). when a tab is clicked.
-				setSelectedTab={setSelectedTab}
-			/>
-			<div className="content">
-				<Header />
-				{/* 
-					Conditional rendering based on the selected tab:
-					- If "Home" is selected, the PostList component is shown
-					- If any other tab is selected, the CreatePost component is shown 
-				*/}
-				{selectedTab === "Home" ? <PostList /> : <CreatePost />}
-				<Footer />
+		<PostListProvider>
+			<div className="app-container">
+				<Sidebar
+					// `selectedTab` prop: This is used to determine which tab is currently active in the Sidebar.
+					selectedTab={selectedTab}
+					// `setSelectedTab` prop: This function is used to update the `selectedTab` state in the parent component (App). when a tab is clicked.
+					setSelectedTab={setSelectedTab}
+				/>
+				<div className="content">
+					<Header />
+					{/* 
+						Conditional rendering based on the selected tab:
+						- If "Home" is selected, the PostList component is shown
+						- If any other tab is selected, the CreatePost component is shown 
+					*/}
+					{selectedTab === "Home" ? <PostList /> : <CreatePost />}
+					<Footer />
+				</div>
 			</div>
-		</div>
+		</PostListProvider>
 	);
 }
 
