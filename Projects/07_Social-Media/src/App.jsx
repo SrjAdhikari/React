e2f12f8bc3adda365836e1af320554ad31983c79
SrjAdhikari <surajadhikari055@gmail.com>
2024-08-24 14:22:30 +0900

    Initialise react app

---
+++ Projects/07_Social-Media/src/App.jsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import './App.css'
+
+function App() {
+    return (
+        <>
+        </>
+    );
+};
+
+export default App;
