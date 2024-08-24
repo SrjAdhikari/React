71c30a061239d0c87bb9cb4afd4690454a2a9f0a
SrjAdhikari <surajadhikari055@gmail.com>
2024-08-24 22:57:48 +0900

    Add PostList component to display list of posts from context

--- Projects/07_Social-Media/src/components/PostList.jsx
+++ Projects/07_Social-Media/src/components/PostList.jsx
@@ -1,12 +1,22 @@
+import { useContext } from "react";
 import Post from "./Post";
+import { PostList as PostData } from "../store/post-list-store";
 
+// Component to display a list of posts
 const PostList = () => {
+	// Retrieve the current list of posts from the PostData context
+	const { postList } = useContext(PostData);
+
+	// Render each post in the postList using the Post component
 	return (
 		<>
-			<Post></Post>
-			<Post></Post>
-			<Post></Post>
-			<Post></Post>
+			{/* 
+				Map over the postList array and render a Post component for each post.
+				Each Post component is provided with a unique key (post.id) and the post data.
+			*/}
+			{postList.map((post) => (
+				<Post key={post.id} post={post} />
+			))}
 		</>
 	);
 };
