b0554ad607139f0c9a151eac2ef5a53ad0acc24d
SrjAdhikari <surajadhikari055@gmail.com>
2024-08-24 23:01:02 +0900

    Add delete functionality and display tags in Post component

--- Projects/07_Social-Media/src/components/Post.jsx
+++ Projects/07_Social-Media/src/components/Post.jsx
@@ -1,18 +1,36 @@
 import React from "react";
+import { useContext } from "react";
+import { MdDelete } from "react-icons/md";
+import { PostList } from "../store/post-list-store";
 
-const Post = () => {
+// Component to display individual post information
+const Post = ({ post }) => {
+	// Retrieve deletePost function from the PostList context
+	const { deletePost } = useContext(PostList);
 	return (
-		<div className="card" style={{ width: "18rem" }}>
-			{/* <img src="..." className="card-img-top" alt="..."></img> */}
+		<div className="card post-card" style={{ width: "20rem" }}>
 			<div className="card-body">
-				<h5 className="card-title">Card title</h5>
-				<p className="card-text">
-					Some quick example text to build on the card title and make
-					up the bulk of the card's content.
-				</p>
-				<a href="#" className="btn btn-primary">
-					Go somewhere
-				</a>
+				<h5 className="card-title">
+					{post.title}
+					{/* Delete button to remove the post */}
+					<span
+						className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
+						onClick={() => deletePost(post.id)}
+					>
+						<MdDelete />
+					</span>
+				</h5>
+				<p className="card-text">{post.body}</p>
+				{/* Display tags associated with the post */}
+				{post.tags.map((tag) => (
+					<span key={tag} className="badge text-bg-primary hashtag">
+						{tag}
+					</span>
+				))}
+				{/* Show the number of reactions the post has received */}
+				<div className="alert alert-success reactions" role="alert">
+					This post has been reacted by {post.reactions} people.
+				</div>
 			</div>
 		</div>
 	);
